2b2ff8633e56523bf914339a26f91e571cf9e14c
yigitnalbant <160471429+yigitnalbant@users.noreply.github.com>
2026-04-21 14:45:07 +0300

    Update s3.tf
    BY

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,1 +1,12 @@
+resource "aws_s3_bucket" "app_bucket" {
+  bucket = "my-unique-app-bucket-896325"
+}
 
+resource "aws_s3_bucket_public_access_block" "public" {
+  bucket = aws_s3_bucket.app_bucket.id
+
+  block_public_acls   = false
+  block_public_policy = false
+  ignore_public_acls  = false
+  restrict_public_buckets = false
+}
