c681073f229db5dd23f5c9f3552b9291cba28061
yigitnalbant <160471429+yigitnalbant@users.noreply.github.com>
2026-04-21 11:20:59 +0300

    test
    Removed required_providers block for AWS provider.

--- main.tf
+++ main.tf
@@ -7,14 +7,6 @@ terraform {
     }
   }
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
 }
